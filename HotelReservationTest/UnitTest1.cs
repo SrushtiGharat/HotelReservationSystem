5c32897e387370f4b672a6381187aa203ffbbdce
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-03 13:47:37 +0530

    Add project files.

---
+++ HotelReservationTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelReservationSystem;
+
+namespace HotelReservationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
