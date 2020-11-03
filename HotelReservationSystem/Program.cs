8b9142bf8cc09f14021ae618344d456e23780454
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-03 13:57:25 +0530

    [Srushti]Add.Hotels to the hotel reservation system

--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -7,6 +7,13 @@ namespace HotelReservationSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Hotel Reservation System");
+
+            HotelSystem hotelSystem = new HotelSystem();
+            hotelSystem.AddHotel(new Hotel("Lakewood", 110));
+            hotelSystem.AddHotel(new Hotel("Bridgewood", 150));
+            hotelSystem.AddHotel(new Hotel("Ridgewood", 220));
+
+            Console.WriteLine("Hotels Loaded......");
         }
     }
 }
--- HotelReservationTest/UnitTest1.cs
+++ HotelReservationTest/UnitTest1.cs
@@ -6,9 +6,16 @@ namespace HotelReservationTest
     [TestClass]
     public class UnitTest1
     {
+        HotelSystem hotelSystem = new HotelSystem();
         [TestMethod]
-        public void TestMethod1()
+        public void Given_NameAndRegularRates_Add_Hotel_To_List()
         {
+            string hotelName = "Lakewood";
+            int ratesForRegularCustomer = 110;
+
+            hotelSystem.AddHotel(new Hotel(hotelName, ratesForRegularCustomer));
+
+            Assert.AreEqual(1, hotelSystem.hotelList.Count);
         }
     }
 }
