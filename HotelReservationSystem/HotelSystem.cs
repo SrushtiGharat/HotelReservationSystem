712755e38d3006dc641db873e11ceb1f47a9134b
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-04 23:17:56 +0530

    [Srushti]Refactor.Take start date and end date

--- HotelReservationSystem/HotelSystem.cs
+++ HotelReservationSystem/HotelSystem.cs
@@ -82,7 +82,7 @@ namespace HotelReservationSystem
 
             foreach (DateTime date in dates)
             {
-                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Saturday)
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
                     weekend++;
                 else
                     weekday++;
